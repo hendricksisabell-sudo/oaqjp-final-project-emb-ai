654735b28b79f3e3474b7397970c3b290bc4f2d4
hendricksisabell-sudo <email@example.comhendricks.isabell@gmail.com>
2026-03-29 11:48:50 -0400

    Initial Commit

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,39 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyse):
+    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    myobj = { "raw_document": { "text": text_to_analyse } }
+    response = requests.post(url, json = myobj, headers=header)
+ 
+    response_dict = json.loads(response.text)
+
+    emotions = response_dict["emotionPredictions"][0]["emotion"]
+
+    anger = emotions.get("anger")
+    disgust = emotions.get("disgust")
+    fear = emotions.get("fear")
+    joy = emotions.get("joy")
+    sadness = emotions.get("sadness")
+
+    emotion_scores = {
+        "anger": anger,
+        "disgust": disgust,
+        "fear": fear,
+        "joy": joy,
+        "sadness": sadness
+    }
+
+    dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+
+    return {
+        "anger": anger,
+        "disgust": disgust,
+        "fear": fear,
+        "joy": joy,
+        "sadness": sadness,
+        "dominant_emotion": dominant_emotion
+    }
+
+print(emotion_detector("I am so happy I am doing this."))
